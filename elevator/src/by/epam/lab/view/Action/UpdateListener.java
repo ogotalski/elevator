027c7f36bd0f2998ff2dfc6f5ef8b08ace8c64e7
Angel <o.gotalsky@gmail.com>
2013-10-24 14:42:02 +0000

    all

---
+++ elevator/src/by/epam/lab/view/Action/UpdateListener.java
@@ -0,0 +1,19 @@
+package by.epam.lab.view.Action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import by.epam.lab.controller.IViewController;
+
+
+public class UpdateListener implements ActionListener {
+	IViewController controller;
+	public UpdateListener(IViewController controller){
+		this.controller = controller;
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		controller.updateView();
+		
+	}
+}
